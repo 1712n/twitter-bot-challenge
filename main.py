f1fc04d225fd1be02ddf1327438d521a066a9296
jalmonter <116984708+jalmonter@users.noreply.github.com>
2022-12-20 20:10:16 -0400

    feat: test MongoDB connection

--- main.py
+++ main.py
@@ -1,4 +1,42 @@
 import os
+import sys
+from loguru import logger
+from pymongo import MongoClient
+from dotenv import load_dotenv
 
-tw_access_token = os.environ.get("TW_ACCESS_TOKEN")
-assert tw_access_token and len(tw_access_token) > 0, 'TW_ACCESS_TOKEN is not set'
+load_dotenv()
+
+logger.remove()
+logger.add(
+    sys.stdout,
+    colorize=True,
+    format="<green>{time:YYYY-MM-DD HH:mm:ss.SS}</green> | <level>{level: <8}</level> | {message}"
+)
+
+logger.info('CryptoCapBot has started!')
+
+
+def get_mongodb_client():
+    address = os.environ["MONGO_DB_ADDRESS"]
+    username = os.environ["MONGODB_USER"]
+    password = os.environ["MONGODB_PASSWORD"]
+
+    uri = f"mongodb+srv://{username}:{password}@{address}"
+
+    return MongoClient(uri)
+
+
+mongodb_client = get_mongodb_client()
+
+for db in mongodb_client.list_database_names():
+    collections = mongodb_client[db].list_collection_names()
+
+    if len(collections) == 0:
+        continue
+
+    logger.debug(
+        "Database '{}' contains {} collection(s): {}",
+        db, len(collections), ','.join(collections)
+    )
+
+logger.info('CryptoCapBot is exiting. Good-bye!')
