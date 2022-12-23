b9b4d648649b902e4cc62f9f290317ef0e1c0c02
Aleksandra <88679504+aleksandra1998e@users.noreply.github.com>
2022-12-23 21:08:26 +0100

    try 5

--- main.py
+++ main.py
@@ -165,8 +165,8 @@ if __name__ == "__main__":
     client = pymongo.MongoClient(uri)
     with client.start_session(causal_consistency = True) as my_session:
         with my_session.start_transaction():
-            print(client.list_database_names())
-            print(client["metrics"].list_collection_names())
+            my_db = client["metrics"]
+            print(my_db.list_collection_names())
 #     ohlcv_db = client.ohlcv_db
 #     posts_db = client.posts_db
 #     print(ohlcv_db.list_collection_names())
