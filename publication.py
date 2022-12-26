9c8ba55af1a0d6968e7ce35d11e88e819ce57b56
Sergei <pavlovergey579@gmail.com>
2022-12-26 15:51:41 +0300

    install environment

---
+++ publication.py
@@ -0,0 +1,18 @@
+import datetime
+import pymongo
+import tweepy
+import os
+from dotenv import load_dotenv, find_dotenv
+
+load_dotenv(find_dotenv())
+
+
+user = os.environ.get("user")
+password = os.environ.get("password")
+cluster_address = os.environ.get("cluster_address")
+
+TW_ACCESS_TOKEN = os.environ.get("TW_ACCESS_TOKEN")
+TW_ACCESS_TOKEN_SECRET = os.environ.get("TW_ACCESS_TOKEN_SECRET")
+TW_CONSUMER_KEY = os.environ.get("TW_CONSUMER_KEY")
+TW_CONSUMER_KEY_SECRET = os.environ.get("TW_CONSUMER_KEY_SECRET")
+
