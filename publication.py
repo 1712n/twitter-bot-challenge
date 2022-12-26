6af0eea9e71e761262c0c34f76cb3505f0da9681
Sergei <pavlovergey579@gmail.com>
2022-12-26 16:24:09 +0300

    upd gitignore

--- publication.py
+++ publication.py
@@ -1,11 +1,9 @@
 import datetime
 import pymongo
-import tweepy
 import os
 from dotenv import load_dotenv, find_dotenv
 
 load_dotenv(find_dotenv())
-
 
 user = os.environ.get("user")
 password = os.environ.get("password")
