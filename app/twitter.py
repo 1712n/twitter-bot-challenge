f62498a00a33e8a76d061ae4a248477a4206de47
Rodrigue Roland CODO <codorol22@gmail.com>
2022-12-27 18:49:27 +0500

    Created app folder and twitter client

---
+++ app/twitter.py
@@ -0,0 +1,18 @@
+import logging
+
+import config
+import tweepy
+
+try:
+    logging.info("Connecting to Twitter...")
+    client = tweepy.Client(
+        consumer_key=config.TW_CONSUMER_KEY,
+        consumer_secret=config.TW_CONSUMER_KEY_SECRET,
+        access_token=config.TW_ACCESS_TOKEN,
+        access_token_secret=config.TW_ACCESS_TOKEN_SECRET,
+    )
+except Exception as e:
+    logging.debug("Error connecting to Twitter:", e)
+
+if __name__ == "__main__":
+    print(client.get_recent_tweets_count("BTC-USD"))
