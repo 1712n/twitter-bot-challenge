954adb5a84e61674905504e724d13db941462693
Rodrigue Roland CODO <codorol22@gmail.com>
2022-12-27 20:55:43 +0500

    Action debugging

--- app/main.py
+++ app/main.py
@@ -1,14 +1,14 @@
 import logging
 
-import db
 import twitter
 from bot import MarketCapBot
+from db import ohlcv_db, posts_db
 
 
 def main():
 
     logging.info("Starting bot...")
-    bot = MarketCapBot(db.ohlcv_db,db.posts_db,twitter.client)
+    bot = MarketCapBot(ohlcv_db,posts_db,twitter.client)
     pair_to_post = bot.get_pair_to_post()
     message = bot.compose_message(pair=pair_to_post)
     bot.post_message(pair=pair_to_post,message=message)
