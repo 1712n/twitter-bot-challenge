ed66687f695aba82e73954d35dcbc5f53510b443
Rodrigue Roland CODO <codorol22@gmail.com>
2022-12-27 19:51:12 +0500

    main.py created

---
+++ app/main.py
@@ -0,0 +1,18 @@
+import logging
+
+import db
+import twitter
+from bot import MarketCapBot
+
+
+def main():
+
+    logging.info("Starting bot...")
+    bot = MarketCapBot(db.ohlcv_db,db.posts_db,twitter.client)
+    pair_to_post = bot.get_pair_to_post()
+    message = bot.compose_message(pair=pair_to_post)
+    print(message)
+    bot.post_message(pair=pair_to_post,message=message)
+
+if __name__ == "__main__":
+    main()
