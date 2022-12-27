68c0c567c7bc9ac3f002143eb3fd33337077607c
jalmonter <116984708+jalmonter@users.noreply.github.com>
2022-12-27 14:09:52 -0400

    feat: get_pair_to_post function

--- tcbot/__main__.py
+++ tcbot/__main__.py
@@ -1,32 +1,26 @@
 from tcbot.logging import logger
-from tcbot.mongodb import get_mongodb_client
+from tcbot.mongodb import get_mongodb_client, get_pair_to_post
 
 
 def main():
-    logger.info("tcbot has started!")
+    logger.info("TCBot has started!")
 
     client = get_mongodb_client()
     database = client.get_database("metrics")
 
-    ohlcv_results = database.ohlcv_db.aggregate([
-        {
-            "$limit": 10
-        }
-    ])
+    logger.info("Getting pair to post...")
+    results = get_pair_to_post(database)
+    results = list(results)
 
-    assert len(list(ohlcv_results)) == 10
-
-    posts_results = database.posts_db.aggregate([
-        {
-            "$limit": 10
-        }
-    ])
-
-    assert len(list(posts_results)) == 10
+    if len(results) > 0:
+        pair_to_post = results[0]
+        logger.info("The pair '{}' needs to be posted!", pair_to_post['_id'])
+    else:
+        logger.info("No pair to post found :(")
 
     client.close()
 
-    logger.info("tcbot is exiting. Good-bye!")
+    logger.info("TCBot is exiting. Good-bye!")
 
 
 if __name__ == "__main__":
--- tcbot/mongodb.py
+++ tcbot/mongodb.py
@@ -1,6 +1,9 @@
 import os
 from dotenv import load_dotenv
 from pymongo import MongoClient
+from pymongo.database import Database
+
+from tcbot.logging import logger
 
 
 def get_mongodb_client():
@@ -13,3 +16,110 @@ def get_mongodb_client():
     uri = f"mongodb+srv://{username}:{password}@{address}"
 
     return MongoClient(uri)
+
+
+def get_pair_to_post(database: Database):
+    logger.debug("Processing documents through aggregation pipelines...")
+
+    return database.ohlcv_db.aggregate([
+        {
+            "$match": {
+                "granularity": "1h"
+            }
+        },
+        {
+            "$group": {
+                "_id": {
+                    "pair_symbol": "$pair_symbol",
+                    "pair_base": "$pair_base",
+                    "market": "$marketVenue"
+                },
+                "total_volume": {
+                    "$sum": {
+                        "$toDouble": "$volume"
+                    }
+                }
+            }
+        },
+        {
+            "$group": {
+                "_id": {
+                    "$concat": [
+                        "$_id.pair_symbol",
+                        "-",
+                        "$_id.pair_base"
+                    ]
+                },
+                "total_volume": {
+                    "$sum": "$total_volume"
+                },
+                "markets": {
+                    "$push": {
+                        "name": "$_id.market",
+                        "volume": "$total_volume"
+                    }
+                }
+            }
+        },
+        {
+            "$sort": {
+                "total_volume": -1
+            }
+        },
+        {
+            "$limit": 100
+        },
+        {
+            "$addFields": {
+                "_id": {
+                    "$toUpper": "$_id"
+                }
+            }
+        },
+        {
+            "$lookup": {
+                "from": "posts_db",
+                "localField": "_id",
+                "foreignField": "pair",
+                "let": {
+                    "pair": "$_id"
+                },
+                "pipeline": [
+                    {
+                        "$match": {
+                            "$expr": {
+                                "$eq": [
+                                    "$pair",
+                                    "$$pair"
+                                ]
+                            }
+                        }
+                    },
+                    {
+                        "$sort": {
+                            "timestamp": -1
+                        }
+                    },
+                    {
+                        "$limit": 1
+                    }
+                ],
+                "as": "last_post"
+            }
+        },
+        {
+            "$unwind": {
+                "path": "$last_post",
+                "preserveNullAndEmptyArrays": True
+            }
+        },
+        {
+            "$sort": {
+                "total_volume": -1,
+                "last_post.timestamp": 1
+            }
+        },
+        {
+          "$limit": 1
+        }
+    ])
