c3b354ee2e07ce823c7f7e7a9bb730c13a6adb7b
Sergey Ugryumov <56644580+Reventaine@users.noreply.github.com>
2022-12-29 18:32:36 +0400

    first contribute

---
+++ config.py
@@ -0,0 +1,11 @@
+import os
+from pymongo import MongoClient
+
+bearer_token = os.environ["TW_BEARER_TOKEN"]
+
+user = os.environ["MONGODB_USER"]
+password = os.environ["MONGODB_PASSWORD"]
+address = os.environ["MONGO_DB_ADDRESS"]
+
+uri = f"mongodb+srv://{user}:{password}@{address}"
+client = MongoClient(uri)
