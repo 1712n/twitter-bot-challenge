c98f35dc0fa2322fd26e9a5bae9fea16fd583334
Egorov Evgeniy <egorovzhenya@gmail.com>
2023-01-01 15:52:46 +0800

    feat: added function that returns the top pairs

--- src/db.py
+++ src/db.py
@@ -15,3 +15,30 @@ try:
 except Exception:
     print("Database connection failed")
     raise
+
+
+def get_top_hundred_pairs(limit: int = 100):
+    ohlcv_docs = db.ohlcv_db.aggregate(
+        [
+            {"$match": {}},
+            {
+                "$group": {
+                    "_id": {"$concat": ["$pair_symbol", "-", "$pair_base"]},
+                    "compound_volume": {
+                        "$sum": {
+                            "$convert": {
+                                "input": "$volume",
+                                "to": "double",
+                                "onError": "Cannot convert input to double",
+                                "onNull": "Input was null or empty",
+                            }
+                        }
+                    },
+                }
+            },
+            {"$sort": {"compound_volume": -1}},
+            {"$limit": limit},
+        ],
+    )
+    top_hundred_pairs = [x["_id"].upper() for x in ohlcv_docs]
+    return top_hundred_pairs
