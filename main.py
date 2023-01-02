405c4987e8289c9deb5df5fa0c131d918aa4c3d9
timujin <newtimujin@gmail.com>
2023-01-02 18:50:13 +0300

    First iteration finished

--- main.py
+++ main.py
@@ -1,22 +1,17 @@
+import sys
+
 import pymongo
-import os
-import datetime
 import tweepy
+import datetime
+import logging
 
-user = os.environ["MONGODB_USER"]
-password = os.environ["MONGODB_PASSWORD"]
-address = os.environ["MONGO_DB_ADDRESS"]
-consumer_key = os.environ["TW_CONSUMER_KEY"]
-consumer_secret = os.environ["TW_CONSUMER_KEY_SECRET"]
-access_token = os.environ["TW_ACCESS_TOKEN"]
-access_token_secret = os.environ["TW_ACCESS_TOKEN_SECRET"]
+from database import Database
+from twitter import Twitter
 
-uri = f"mongodb+srv://{user}:{password}@{address}"
-client = pymongo.MongoClient(uri)
+use_twitter = True  # change to True if you want the tweet to be actually posted
 
-if __name__ == '__main__':
-    ############### part 1 of the algorithm
 
+def get_top_pairs(db):
     select_granularity = {"$match": {"granularity": "1h"}}
     limit_data = {
         "$group": {
@@ -35,17 +30,17 @@ if __name__ == '__main__':
     }
     top_100 = {"$limit": 100}
 
-    res = client["metrics"]["ohlcv_db"].aggregate([
+    res = db.ohlcv().aggregate([
         select_granularity,
         limit_data,
         sort_data,
         top_100
     ])
 
-    top_pairs = [(x["_id"]["pair"], x["volume"]) for x in res]
+    return [(x["_id"]["pair"], x["volume"]) for x in res]
 
-    ####################### part 2 of the algorithm
 
+def get_latest_posts(db, top_pairs):
     match_pairs = {
         "$match": {
             "pair": {"$in": [x[0] for x in top_pairs]}
@@ -65,48 +60,44 @@ if __name__ == '__main__':
         }
     }
 
-    res = client["metrics"]["posts_db"].aggregate([
+    res = db.posts().aggregate([
         match_pairs,
         find_latest,
         sort_latest,
     ])
 
-    latest_pairs = [(x["_id"], x["time"]) for x in res]
+    return [(x["_id"], x["time"]) for x in res]
 
-    #################### Selecting pair_to_post
 
+def select_pair_to_post(top_pairs, latest_pairs):
     # The instructions are unclear, the exact algorithm to choose the pair to post is not given
     # So I'm just going to pick the biggest pair by volume that's in latest_pairs
-
     latest_pairs_ids = [x[0] for x in latest_pairs]
-    pair_to_post = None
     for (pair, volume) in top_pairs:
         if pair in latest_pairs_ids:
-            pair_to_post = (pair, volume)
-            break
+            return pair, volume
 
-    # print(pair_to_post)
 
-    ############### Composing message
-
+def compose_message(db, pair_to_post):
     pair = pair_to_post[0]
     symbol = pair.split('-')[0].lower()
     base = pair.split('-')[1].lower()
 
+    select_granularity = {"$match": {"granularity": "1h"}}
     find_the_pair = {
         "$match": {
-            "pair_symbol":symbol,
-            "pair_base":base
+            "pair_symbol": symbol,
+            "pair_base": base
         }
     }
     sort_by_timestamp = {
         "$sort": {
-                "timestamp": pymongo.DESCENDING
+            "timestamp": pymongo.DESCENDING
         }
 
     }
 
-    tickers = client["metrics"]["ohlcv_db"].aggregate([
+    tickers = db.ohlcv().aggregate([
         select_granularity,
         find_the_pair,
         sort_by_timestamp
@@ -129,52 +120,86 @@ if __name__ == '__main__':
 
     total_volume = sum(market_venues.values())
     items = sorted(market_venues.items(), key=lambda x: x[1], reverse=True)[:5]
-    percentages = [(key, (value/total_volume)) for (key, value) in items]
+    percentages = [(key, (value / total_volume)) for (key, value) in items]
     others = 1.0 - sum([x[1] for x in percentages])
-    #print(percentages)
-    #print(others)
     response = f"Top Market Venues for {pair}:\n"
     for (market, value) in percentages:
-        percentage = str(round(value*100, 1))
+        percentage = str(round(value * 100, 1))
         venue = market.capitalize()
         response += f"{venue}: {percentage}%\n"
-    percentage = str(round(others*100, 1))
+    percentage = str(round(others * 100, 1))
     response += f"Others: {percentage}%"
-    print(response)
+    return response
 
-    ############ adding the tweet to posts
 
+def post_message_to_db(db, pair, message):
     post = {
         "pair": pair,
-        "tweet_text": response,
+        "tweet_text": message,
         "time": datetime.datetime.fromisoformat(datetime.datetime.utcnow().strftime("%Y-%m-%d %H:%M"))
     }
-    client["metrics"]["posts_db"].insert_one(post)
+    db.posts().insert_one(post)
 
-    ##### posting the tweet
 
-    use_twitter = True
+def post_message_to_twitter(db, pair, message):
+    parent = db.posts().find({
+        'pair': pair,
+        'tweet_id': {
+            '$exists': True
+        }
+    }) \
+        .sort('time', pymongo.DESCENDING) \
+        .limit(1)
+
+    parent = list(parent)
+
+    input("press to continue...") # to work around a problem with Twitter being blocked in my country
+
+    if len(parent) == 0:
+        Twitter().post_new_tweet(message)
+    else:
+        Twitter().post_reply(message, parent[0]["tweet_id"])
+
+
+if __name__ == '__main__':
+    db = Database()
+
+    top_pairs = None
+    try:
+        top_pairs = get_top_pairs(db)
+    except pymongo.errors.PyMongoError as e:
+        logging.error(f"Error reading ohlcv: {e}")
+        sys.exit(1)
+
+    latest_pairs = None
+    try:
+        latest_pairs = get_latest_posts(db, top_pairs)
+    except pymongo.errors.PyMongoError as e:
+        logging.error(f"Error reading posts: {e}")
+        sys.exit(1)
+
+    pair_to_post = select_pair_to_post(top_pairs, latest_pairs)
+
+    message = None
+    try:
+        message = compose_message(db, pair_to_post)
+    except pymongo.errors.PyMongoError as e:
+        logging.error(f"Error reading ohlcv: {e}")
+        sys.exit(1)
+
+    try:
+        post_message_to_db(db, pair_to_post, message)
+    except pymongo.errors.PyMongoError as e:
+        logging.error(f"Error adding to posts: {e}")
+        sys.exit(1)
 
     if use_twitter:
-        twitter = tweepy.Client(
-            consumer_key=consumer_key,
-            consumer_secret=consumer_secret,
-            access_token=access_token,
-            access_token_secret=access_token_secret,
-        )
+        try:
+            post_message_to_twitter(db, pair_to_post, message)
+        except pymongo.errors.PyMongoError as e:
+            logging.error(f"Error reading posts: {e}")
+            sys.exit(1)
+        except tweepy.errors.TweepyException as e:
+            logging.error(f"Error posting to Twitter: {e}")
+            sys.exit(1)
 
-        parent = client["metrics"]["posts_db"].find({
-            'pair':pair,
-            'tweet_id': {
-                '$exists': True
-            }
-        })\
-            .sort('time',pymongo.DESCENDING)\
-            .limit(1)
-        parent = list(parent)
-        print(parent[0])
-        #input("press to continue...") ## to work around a problem with Twitter being blocked in my country
-        if parent is []:
-            twitter.create_tweet(text=response)
-        else:
-            twitter.create_tweet(text=response, in_reply_to_tweet_id=parent[0]["tweet_id"])
