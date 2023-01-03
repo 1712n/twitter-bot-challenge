b5eb560a2e27d4e4600ec874f09b3324a00bfb20
Egorov Evgeniy <egorovzhenya@gmail.com>
2023-01-03 22:01:46 +0800

    feat: added func for inserting a new doc

--- src/db.py
+++ src/db.py
@@ -4,6 +4,7 @@ from typing import List
 from dotenv import load_dotenv
 from pymongo import MongoClient
 from pymongo.errors import PyMongoError
+from datetime import datetime
 
 load_dotenv()
 
@@ -22,7 +23,7 @@ except PyMongoError as err:
 def get_top_pairs_by_volume(limit: int = 100, extra_condition={}):
     logging.info("Getting pairs with top compound volume.")
     try:
-        query_result = db.ohlcv_db.aggregate(
+        fetched_data = db.ohlcv_db.aggregate(
             [
                 {
                     "$group": {
@@ -50,7 +51,7 @@ def get_top_pairs_by_volume(limit: int = 100, extra_condition={}):
         )
     except PyMongoError as err:
         logging.error("Faild to get data from database: %s", err)
-    pairs_w_top_volume = [pair["_id"] for pair in query_result]
+    pairs_w_top_volume = [pair["_id"] for pair in fetched_data]
     logging.info("Top pairs are collected")
     return pairs_w_top_volume
 
@@ -58,7 +59,7 @@ def get_top_pairs_by_volume(limit: int = 100, extra_condition={}):
 def get_pair_to_post(pairs: List):
     logging.info("Selecting pairs with oldest timestamp")
     try:
-        query_result = db.posts_db.aggregate(
+        fetched_data = db.posts_db.aggregate(
             [
                 {"$match": {"pair": {"$in": pairs}}},
                 {
@@ -79,7 +80,7 @@ def get_pair_to_post(pairs: List):
         )
     except PyMongoError as err:
         logging.error("Faild to get data from database: %s", err)
-    pairs_w_oldest_timetamp = [pair["_id"] for pair in query_result]
+    pairs_w_oldest_timetamp = [pair["_id"] for pair in fetched_data]
     logging.info("Selecting pair with max compound volume among them")
     max_volume_among_oldest = get_top_pairs_by_volume(
         limit=1, extra_condition={"_id": {"$in": pairs_w_oldest_timetamp}}
@@ -92,7 +93,7 @@ def get_pair_to_post(pairs: List):
 def get_message_to_post(pair: str):
     logging.info("Getting coresponding market vanues for selected pair")
     try:
-        pair_markets = list(
+        fetched_data = list(
             db.ohlcv_db.aggregate(
                 [
                     {
@@ -132,15 +133,15 @@ def get_message_to_post(pair: str):
         logging.error("Faild to get data from database: %s", err)
     logging.info("Composing message to post")
     message_components = [f"Top Market Venues for {pair}:", ]
-    cmpd_volume = sum(market["volume"] for market in pair_markets)
-    for i, mkt in enumerate(pair_markets):
+    cmpd_volume = sum(market["volume"] for market in fetched_data)
+    for i, mkt in enumerate(fetched_data):
         if i == 5:
             break
         message_components.append(
             f"{mkt['_id'].capitalize()} {(mkt['volume']/cmpd_volume)*100:.2f}%"
         )
-    if len(pair_markets) > 5:
-        other_markets_volume = sum(mkt["volume"] for mkt in pair_markets[5:])
+    if len(fetched_data) > 5:
+        other_markets_volume = sum(mkt["volume"] for mkt in fetched_data[5:])
         message_components.append(
             f"Others {(other_markets_volume/cmpd_volume)*100:.2f}%"
         )
@@ -152,7 +153,7 @@ def get_message_to_post(pair: str):
 def get_origin_tweet_id(pair: str):
     logging.info("Checking previous posts for given pair")
     try:
-        query_result = list(db.posts_db.aggregate(
+        fetched_data = list(db.posts_db.aggregate(
             [
                 {"$match": {"pair": pair}},
                 {
@@ -167,9 +168,24 @@ def get_origin_tweet_id(pair: str):
     except PyMongoError as err:
         logging.error("Faild to get data from database: %s", err)
     origin_tweet_id = None
-    if query_result:
+    if fetched_data:
         logging.info(
             "%s was published earlier, the thread will continue", pair
         )
-        origin_tweet_id = query_result[0]["tweet_id"]
+        origin_tweet_id = fetched_data[0]["tweet_id"]
     return origin_tweet_id
+
+
+def add_new_post_to_db(pair: str, tweet_id: str, text: str):
+    try:
+        inserted_document = db.posts_db.insert_one(
+            {
+                "pair": pair,
+                "tweet_id": tweet_id,
+                "text": text,
+                "time": datetime.now(),
+            }
+        )
+    except PyMongoError as err:
+        logging.error("Faild to insert data in database: %s", err)
+    return inserted_document.inserted_id
