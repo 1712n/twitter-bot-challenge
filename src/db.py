c5aa5ecf753e3e15e7169199431ea6290e707c78
Egorov Evgeniy <egorovzhenya@gmail.com>
2023-01-03 20:31:07 +0800

    feat: rebuilt new_tweet func, added err handling

--- src/db.py
+++ src/db.py
@@ -3,50 +3,53 @@ import logging
 from typing import List
 from dotenv import load_dotenv
 from pymongo import MongoClient
+from pymongo.errors import PyMongoError
 
 load_dotenv()
 
 user = os.environ["MONGODB_USER"]
 password = os.environ["MONGODB_PASSWORD"]
 address = os.environ["MONGO_DB_ADDRESS"]
+uri = f"mongodb+srv://{user}:{password}@{address}"
 
 try:
-    uri = f"mongodb+srv://{user}:{password}@{address}"
     client = MongoClient(uri)
     db = client.metrics
-except Exception:
-    print("Database connection failed")
-    raise
+except PyMongoError as err:
+    logging.error("Database connection failure: %s", err)
 
 
 def get_top_pairs_by_volume(limit: int = 100, extra_condition={}):
-    logging.info("Getting pairs with top compound volume")
-    query_result = db.ohlcv_db.aggregate(
-        [
-            {
-                "$group": {
-                    "_id": {
-                        "$toUpper": {"$concat": [
-                            "$pair_symbol", "-", "$pair_base"
-                        ]}
-                    },
-                    "compound_volume": {
-                        "$sum": {
-                            "$convert": {
-                                "input": "$volume",
-                                "to": "double",
-                                "onError": "Cannot convert to double",
-                                "onNull": "Input was null or empty",
+    logging.info("Getting pairs with top compound volume.")
+    try:
+        query_result = db.ohlcv_db.aggregate(
+            [
+                {
+                    "$group": {
+                        "_id": {
+                            "$toUpper": {"$concat": [
+                                "$pair_symbol", "-", "$pair_base"
+                            ]}
+                        },
+                        "compound_volume": {
+                            "$sum": {
+                                "$convert": {
+                                    "input": "$volume",
+                                    "to": "double",
+                                    "onError": "Cannot convert to double",
+                                    "onNull": "Input was null or empty",
+                                }
                             }
-                        }
-                    },
-                }
-            },
-            {"$match": extra_condition},
-            {"$sort": {"compound_volume": -1}},
-            {"$limit": limit},
-        ],
-    )
+                        },
+                    }
+                },
+                {"$match": extra_condition},
+                {"$sort": {"compound_volume": -1}},
+                {"$limit": limit},
+            ],
+        )
+    except PyMongoError as err:
+        logging.error("Faild to get data from database: %s", err)
     pairs_w_top_volume = [pair["_id"] for pair in query_result]
     logging.info("Top pairs are collected")
     return pairs_w_top_volume
@@ -54,25 +57,28 @@ def get_top_pairs_by_volume(limit: int = 100, extra_condition={}):
 
 def get_pair_to_post(pairs: List):
     logging.info("Selecting pairs with oldest timestamp")
-    query_result = db.posts_db.aggregate(
-        [
-            {"$match": {"pair": {"$in": pairs}}},
-            {
-                "$project": {
-                    "pair": 1,
-                    "timestamp": {"$ifNull": ["$time", "$timestamp"]},
-                }
-            },
-            {
-                "$group": {
-                    "_id": "$pair",
-                    "latest post": {"$min": "$timestamp"}
-                }
-            },
-            {"$sort": {"latest post": 1}},
-            {"$limit": 5},
-        ]
-    )
+    try:
+        query_result = db.posts_db.aggregate(
+            [
+                {"$match": {"pair": {"$in": pairs}}},
+                {
+                    "$project": {
+                        "pair": 1,
+                        "timestamp": {"$ifNull": ["$time", "$timestamp"]},
+                    }
+                },
+                {
+                    "$group": {
+                        "_id": "$pair",
+                        "latest post": {"$min": "$timestamp"}
+                    }
+                },
+                {"$sort": {"latest post": 1}},
+                {"$limit": 5},
+            ]
+        )
+    except PyMongoError as err:
+        logging.error("Faild to get data from database: %s", err)
     pairs_w_oldest_timetamp = [pair["_id"] for pair in query_result]
     logging.info("Selecting pair with max compound volume among them")
     max_volume_among_oldest = get_top_pairs_by_volume(
@@ -85,40 +91,45 @@ def get_pair_to_post(pairs: List):
 
 def get_message_to_post(pair: str):
     logging.info("Getting coresponding market vanues for selected pair")
-    pair_markets = list(
-        db.ohlcv_db.aggregate(
-            [
-                {
-                    "$project": {
-                        "pair": {
-                            "$toUpper": {
-                                "$concat": ["$pair_symbol", "-", "$pair_base"]
-                            }
-                        },
-                        "marketVenue": 1,
-                        "volume": 1,
-                    }
-                },
-                {"$match": {"pair": pair}},
-                {
-                    "$group": {
-                        "_id": "$marketVenue",
-                        "volume": {
-                            "$sum": {
-                                "$convert": {
-                                    "input": "$volume",
-                                    "to": "double",
-                                    "onError": "Cannot convert to double",
-                                    "onNull": "Input was null or empty",
+    try:
+        pair_markets = list(
+            db.ohlcv_db.aggregate(
+                [
+                    {
+                        "$project": {
+                            "pair": {
+                                "$toUpper": {
+                                    "$concat": [
+                                        "$pair_symbol", "-", "$pair_base"
+                                    ]
                                 }
-                            }
-                        },
-                    }
-                },
-                {"$sort": {"volume": -1}},
-            ]
+                            },
+                            "marketVenue": 1,
+                            "volume": 1,
+                        }
+                    },
+                    {"$match": {"pair": pair}},
+                    {
+                        "$group": {
+                            "_id": "$marketVenue",
+                            "volume": {
+                                "$sum": {
+                                    "$convert": {
+                                        "input": "$volume",
+                                        "to": "double",
+                                        "onError": "Cannot convert to double",
+                                        "onNull": "Input was null or empty",
+                                    }
+                                }
+                            },
+                        }
+                    },
+                    {"$sort": {"volume": -1}},
+                ]
+            )
         )
-    )
+    except PyMongoError as err:
+        logging.error("Faild to get data from database: %s", err)
     logging.info("Composing message to post")
     message_components = [f"Top Market Venues for {pair}:", ]
     cmpd_volume = sum(market["volume"] for market in pair_markets)
@@ -136,3 +147,29 @@ def get_message_to_post(pair: str):
     message_to_post = "\n".join(message_components)
     logging.info("Message is ready for posting")
     return message_to_post
+
+
+def get_origin_tweet_id(pair: str):
+    logging.info("Checking previous posts for given pair")
+    try:
+        query_result = list(db.posts_db.aggregate(
+            [
+                {"$match": {"pair": pair}},
+                {
+                    "$project": {
+                        "pair": 1,
+                        "tweet_id": {"$ifNull": ["$tweet_id", None]}
+                    }
+                },
+                {"$limit": 1}
+            ]
+        ))
+    except PyMongoError as err:
+        logging.error("Faild to get data from database: %s", err)
+    origin_tweet_id = None
+    if query_result:
+        logging.info(
+            "%s was published earlier, the thread will continue", pair
+        )
+        origin_tweet_id = query_result[0]["tweet_id"]
+    return origin_tweet_id
--- src/twitter_bot.py
+++ src/twitter_bot.py
@@ -2,11 +2,9 @@ import os
 import tweepy
 import logging
 from dotenv import load_dotenv
-
-from db import db
+from tweepy.errors import TweepyException
 
 load_dotenv()
-
 
 try:
     client = tweepy.Client(
@@ -15,33 +13,16 @@ try:
         access_token=os.environ["TW_ACCESS_TOKEN"],
         access_token_secret=os.environ["TW_ACCESS_TOKEN_SECRET"]
     )
-except Exception:
-    print("Twitter connection failed")
-    raise
+except TweepyException as err:
+    logging.error("Twitter connection failed: %s", err)
 
 
-def new_tweet(pair: str, message_to_post: str):
-    logging.info("Checking previous posts for given pair")
-    query_result = list(db.posts_db.aggregate(
-        [
-            {"$match": {"pair": pair}},
-            {
-                "$project": {
-                    "pair": 1,
-                    "tweet_id": {"$ifNull": ["$tweet_id", None]}
-                }
-            },
-            {"$limit": 1}
-        ]
-    ))
-    origin_tweet_id = None
-    if query_result:
-        logging.info("f{pair} was published earlier, the thread will continue")
-        origin_tweet_id = query_result[0]["tweet_id"]
-        if not origin_tweet_id:
-            logging.info(
-                "Origin tweet id isn't specified in db. Getting from twitter.."
-                )
+def new_tweet(pair: str, text: str, origin_tweet_id: str | None):
+    if not origin_tweet_id:
+        logging.info(
+            "Origin tweet id isn't specified in db. Getting from twitter.."
+            )
+        try:
             bot_id = client.get_me().data.id
             paginator = tweepy.Paginator(
                 client.get_users_tweets,
@@ -50,14 +31,18 @@ def new_tweet(pair: str, message_to_post: str):
                 max_results=100,
                 user_auth=True
             )
-            for page in paginator.flatten():
-                if pair in page.data["text"]:
-                    origin_tweet_id = page.data["id"]
-                    logging.info("Origin tweet id is found")
-                    break
+        except TweepyException as err:
+            logging.error("Failed to get data from Twitter: %s", err)
+        for page in paginator.flatten():
+            if pair in page.data["text"]:
+                origin_tweet_id = page.data["id"]
+                logging.info("Origin tweet id is found")
+                break
     logging.info("Creating new tweet..")
-    new_tweet = client.create_tweet(
-        text=message_to_post, in_reply_to_tweet_id=origin_tweet_id
-        )
-    logging.info("New tweet is created!")
+    try:
+        new_tweet = client.create_tweet(
+            text=text, in_reply_to_tweet_id=origin_tweet_id
+            )
+    except TweepyException as err:
+        logging.error("Failed to publish new tweet: %s", err)
     return new_tweet.data["id"]
