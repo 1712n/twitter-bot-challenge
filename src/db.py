6db14dd5d4d4549f33d63a774d8425a22e24b6b3
Egorov Evgeniy <egorovzhenya@gmail.com>
2023-01-04 11:06:22 +0800

    feat: added func for getting db client

--- src/db.py
+++ src/db.py
@@ -1,28 +1,26 @@
-import os
 import logging
 from typing import List
-from dotenv import load_dotenv
 from pymongo import MongoClient
+from pymongo.database import Database
 from pymongo.errors import PyMongoError
 from datetime import datetime
 
-load_dotenv()
 
-user = os.environ["MONGODB_USER"]
-password = os.environ["MONGODB_PASSWORD"]
-address = os.environ["MONGO_DB_ADDRESS"]
-uri = f"mongodb+srv://{user}:{password}@{address}"
-
-try:
-    client = MongoClient(uri)
-    db = client.metrics
-except PyMongoError as err:
-    logging.error("Database connection failure: %s", err)
-    raise
+def get_db_client(uri: str):
+    logging.info("Connecting to Database..")
+    try:
+        client = MongoClient(uri)
+    except PyMongoError as err:
+        logging.error("Database connection failure: %s", err)
+        raise
+    logging.info("Сonnected to Database!")
+    return client
 
 
-def get_top_pairs_by_volume(limit: int = 100, extra_condition={}):
-    logging.info("Getting pairs with top compound volume.")
+def get_top_pairs_by_volume(
+    db: Database, limit: int = 100, extra_condition={}
+):
+    logging.info("Getting pairs with top compound volume..")
     try:
         fetched_data = db.ohlcv_db.aggregate(
             [
@@ -54,12 +52,12 @@ def get_top_pairs_by_volume(limit: int = 100, extra_condition={}):
         logging.error("Faild to get data from database: %s", err)
         raise
     pairs_w_top_volume = [pair["_id"] for pair in fetched_data]
-    logging.info("Top pairs are collected")
+    logging.info("Top pairs are collected!")
     return pairs_w_top_volume
 
 
-def get_pair_to_post(pairs: List):
-    logging.info("Selecting pairs with oldest timestamp")
+def get_pair_to_post(db: Database, pairs: List):
+    logging.info("Selecting pairs with oldest timestamp..")
     try:
         fetched_data = db.posts_db.aggregate(
             [
@@ -84,17 +82,19 @@ def get_pair_to_post(pairs: List):
         logging.error("Faild to get data from database: %s", err)
         raise
     pairs_w_oldest_timetamp = [pair["_id"] for pair in fetched_data]
-    logging.info("Selecting pair with max compound volume among them")
+    logging.info("Selecting pair with max compound volume among them..")
     max_volume_among_oldest = get_top_pairs_by_volume(
-        limit=1, extra_condition={"_id": {"$in": pairs_w_oldest_timetamp}}
+        db=db,
+        limit=1,
+        extra_condition={"_id": {"$in": pairs_w_oldest_timetamp}}
     )
-    logging.info("Getting a pair to post")
+    logging.info("Getting a pair to post..")
     pair_to_post = max_volume_among_oldest[0]
     return pair_to_post
 
 
-def get_message_to_post(pair: str):
-    logging.info("Getting coresponding market vanues for selected pair")
+def get_message_to_post(db: Database, pair: str):
+    logging.info("Getting coresponding market vanues for selected pair..")
     try:
         fetched_data = list(
             db.ohlcv_db.aggregate(
@@ -135,7 +135,7 @@ def get_message_to_post(pair: str):
     except PyMongoError as err:
         logging.error("Faild to get data from database: %s", err)
         raise
-    logging.info("Composing message to post")
+    logging.info("Composing message to post..")
     message_components = [f"Top Market Venues for {pair}:", ]
     cmpd_volume = sum(market["volume"] for market in fetched_data)
     for i, mkt in enumerate(fetched_data):
@@ -150,11 +150,11 @@ def get_message_to_post(pair: str):
             f"Others {(other_markets_volume/cmpd_volume)*100:.2f}%"
         )
     message_to_post = "\n".join(message_components)
-    logging.info("Message is ready for posting")
+    logging.info("Message is ready for posting!")
     return message_to_post
 
 
-def get_origin_tweet_id(pair: str):
+def get_origin_tweet_id(db: Database, pair: str):
     logging.info("Checking previous posts for given pair")
     try:
         fetched_data = list(db.posts_db.aggregate(
@@ -175,13 +175,14 @@ def get_origin_tweet_id(pair: str):
     origin_tweet_id = None
     if fetched_data:
         logging.info(
-            "%s was published earlier, the thread will continue", pair
+            "%s was published earlier, the thread will continue..", pair
         )
         origin_tweet_id = fetched_data[0]["tweet_id"]
     return origin_tweet_id
 
 
-def add_new_post_to_db(pair: str, tweet_id: str, text: str):
+def add_new_post_to_db(db: Database, pair: str, tweet_id: str, text: str):
+    logging.info("Inserting new post to database..")
     try:
         inserted_document = db.posts_db.insert_one(
             {
