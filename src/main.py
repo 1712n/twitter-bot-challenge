e0de6f0de829bfa340c74639f1b7890be9b78e80
Egorov Evgeniy <egorovzhenya@gmail.com>
2023-01-04 11:11:41 +0800

    feat: passing env variables via main func

--- src/main.py
+++ src/main.py
@@ -1,34 +1,61 @@
+import os
 import logging
+from dotenv import load_dotenv
 from db import (
+    get_db_client,
     get_top_pairs_by_volume,
     get_pair_to_post,
     get_message_to_post,
     get_origin_tweet_id,
     add_new_post_to_db
 )
-from twitter_bot import new_tweet
+from twitter_bot import (
+    get_twitter_client,
+    new_tweet
+)
 
 logger = logging.getLogger(__name__)
-
 logging.basicConfig(
     format="%(asctime)s %(name)5s: %(lineno)3s: %(levelname)s >> %(message)s",
-    level=logging.INFO,
+    level=logging.INFO
 )
+
+load_dotenv()
+
+user = os.environ["MONGODB_USER"]
+password = os.environ["MONGODB_PASSWORD"]
+address = os.environ["MONGO_DB_ADDRESS"]
+uri = f"mongodb+srv://{user}:{password}@{address}"
+
+consumer_key = os.environ["TW_CONSUMER_KEY"]
+consumer_secret = os.environ["TW_CONSUMER_KEY_SECRET"]
+access_token = os.environ["TW_ACCESS_TOKEN"]
+access_token_secret = os.environ["TW_ACCESS_TOKEN_SECRET"]
 
 
 def main():
     logging.info("Starting script..")
-    top_pairs = get_top_pairs_by_volume()
-    pair_to_post = get_pair_to_post(pairs=top_pairs)
-    message_to_post = get_message_to_post(pair=pair_to_post)
-    origin_tweet_id = get_origin_tweet_id(pair=pair_to_post)
+    db_client = get_db_client(uri=uri)
+    db = db_client.metrics
+    twitter_client = get_twitter_client(
+        consumer_key,
+        consumer_secret,
+        access_token,
+        access_token_secret
+    )
+    top_pairs = get_top_pairs_by_volume(db=db)
+    pair_to_post = get_pair_to_post(db=db, pairs=top_pairs)
+    message_to_post = get_message_to_post(db=db, pair=pair_to_post)
+    origin_tweet_id = get_origin_tweet_id(db=db, pair=pair_to_post)
     new_tweet_id = new_tweet(
+        client=twitter_client,
         pair=pair_to_post,
         text=message_to_post,
         origin_tweet_id=origin_tweet_id
     )
     logging.info("New tweet with id=%s was created!", new_tweet_id)
     new_post_id = add_new_post_to_db(
+        db=db,
         pair=pair_to_post,
         tweet_id=new_tweet_id,
         text=message_to_post
