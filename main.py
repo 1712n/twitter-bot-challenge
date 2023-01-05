a7705863a451ddf0691e7bb2ea943e647b771661
Braingenius2 <braingenius2@gmail.com>
2023-01-05 16:39:58 +0300

    update yml file and main.py
    included some dependencies in yml file, triedto work out logic and querying the database and sorting as defined in the challenge

--- main.py
+++ main.py
@@ -1,18 +1,23 @@
 import os
 from pymongo import MongoClient
-from twitter import Twitter
+import tweepy
 
-# Get environment variables
-TW_ACCESS_TOKEN = os.environ['TW_ACCESS_TOKEN']
-TW_ACCESS_TOKEN_SECRET = os.environ['TW_ACCESS_TOKEN_SECRET']
-TW_CONSUMER_KEY = os.environ['TW_CONSUMER_KEY']
-TW_CONSUMER_KEY_SECRET = os.environ['TW_CONSUMER_KEY_SECRET']
-mongodb_user = os.environ['MONGODB_USER']
-mongodb_password = os.environ['MONGODB_PASSWORD']
-mongo_db_address = os.environ['MONGO_DB_ADDRESS']
+# Authenticate with the Twitter API using OAuth 1.0a
+auth = tweepy.OAuth1UserHandler(
+    consumer_key=os.environ['TW_CONSUMER_KEY'],
+    consumer_secret=os.environ['TW_CONSUMER_KEY_SECRET'],
+    access_token=os.environ['TW_ACCESS_TOKEN'],
+    access_token_secret=os.environ['TW_ACCESS_TOKEN_SECRET'])
+api = tweepy.API(auth)
 
 # Connect to MongoDB
-client = MongoClient(f'mongodb+srv://{mongodb_user}:{mongodb_password}@{mongodb_cluster_address}')
+user = os.environ["MONGODB_USER"]
+password = os.environ["MONGODB_PASSWORD"]
+address = os.environ["MONGO_DB_ADDRESS"]
+uri = f"mongodb+srv://{user}:{password}@{address}"
+client = MongoClient(uri)
+
+#fuctionality
 ohlcv_db = client['ohlcv_db']
 posts_db = client['posts_db']
 
@@ -22,5 +27,7 @@ top_pairs = ohlcv_db.pair.find().sort('volume', -1).limit(100)
 # Query posts_db for the latest documents corresponding to those 100 pairs
 latest_posts = posts_db.posts.find({'pair': {'$in': top_pairs}})
 
-# Sort results by the oldest timestamp to find the pairs that haven't been posted for a while
-sorted_posts = latest_posts.sort('timestamp', 1)
+# Sort the results by the oldest timestamp to find the pairs that haven't been posted for a while,
+# then sort by corresponding volume to find the biggest markets among them
+pair_to_post = sorted(latest_posts, key=lambda x: x['timestamp'])[0]
+pair_to_post = sorted(pair_to_post, key=lambda x: x['volume'], reverse=True)[0]
