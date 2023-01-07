4e6d3423786cdb6ff66be08ee5a91d167b804872
Braingenius2 <braingenius2@gmail.com>
2023-01-07 15:46:39 +0300

    Update main.py
    modified to leverage MongoDB time series optimizations by using a capped collection, and also using a tailable cursor to efficiently read docs in the capped collection

--- main.py
+++ main.py
@@ -1,33 +1,56 @@
-import os
-from pymongo import MongoClient
 import tweepy
+from pymongo import MongoClient
+import os
 
-# Authenticate with the Twitter API using OAuth 1.0a
-auth = tweepy.OAuth1UserHandler(
-    consumer_key=os.environ['TW_CONSUMER_KEY'],
-    consumer_secret=os.environ['TW_CONSUMER_KEY_SECRET'],
-    access_token=os.environ['TW_ACCESS_TOKEN'],
-    access_token_secret=os.environ['TW_ACCESS_TOKEN_SECRET'])
-api = tweepy.API(auth)
+def main():
+    try:
+        # Authenticate with the Twitter API using OAuth 1.0a
+        auth = tweepy.OAuth1UserHandler(
+            consumer_key=os.environ['TW_CONSUMER_KEY'],
+            consumer_secret=os.environ['TW_CONSUMER_SECRET'],
+            access_token=os.environ['TW_ACCESS_TOKEN'],
+            access_token_secret=os.environ['TW_ACCESS_TOKEN_SECRET'])
+        api = tweepy.API(auth)
 
-# Connect to MongoDB
-user = os.environ["MONGODB_USER"]
-password = os.environ["MONGODB_PASSWORD"]
-address = os.environ["MONGO_DB_ADDRESS"]
-uri = f"mongodb+srv://{user}:{password}@{address}"
-client = MongoClient(uri)
+        # Connect to the MongoDB database
+        user = os.environ["MONGODB_USER"]
+        password = os.environ["MONGODB_PASSWORD"]
+        address = os.environ["MONGO_DB_ADDRESS"]
 
-#fuctionality
-ohlcv_db = client['ohlcv_db']
-posts_db = client['posts_db']
+        uri = f"mongodb+srv://{user}:{password}@{address}"
+        client = MongoClient(uri)
+        ohlcv_db = client['ohlcv_db']
+        posts_db = client['posts_db']
 
-# Query ohlcv_db for the top 100 pairs by compound volume
-top_pairs = ohlcv_db.pair.find().sort('volume', -1).limit(100)
+        # Use a tailable cursor to efficiently read the latest documents in the ohlcv_db capped collection
+        cursor = ohlcv_db.find(cursor_type=pymongo.cursor.CursorType.TAILABLE).sort([('$natural', -1)]).limit(100)
 
-# Query posts_db for the latest documents corresponding to those 100 pairs
-latest_posts = posts_db.posts.find({'pair': {'$in': top_pairs}})
+        # Continuously process new documents as they are added to the collection
+        while cursor.alive:
+            for doc in cursor:
+                # Check if the document corresponds to a pair that hasn't been tweeted about recently
+                latest_post = posts_db.find_one({'pair': doc['pair']})
+                if not latest_post or doc['timestamp'] > latest_post['timestamp']:
+                    # Compose the message_to_post
+                    message_to_post = f"Top Market Venues for {doc['pair']}:\n"
+                    for market, volume in doc['market_volumes'].items():
+                        message_to_post += f"{market}: {volume:.2f}%\n"
 
-# Sort the results by the oldest timestamp to find the pairs that haven't been posted for a while,
-# then sort by corresponding volume to find the biggest markets among them
-pair_to_post = sorted(latest_posts, key=lambda x: x['timestamp'])[0]
-pair_to_post = sorted(pair_to_post, key=lambda x: x['volume'], reverse=True)[0]
+                    # Check if the message_to_post already exists in the posts collection
+                    existing_post = posts_db.find_one({'message': message_to_post})
+                    if existing_post:
+                        # If it does, post the tweet to the corresponding Twitter thread
+                        tweet = api.update_status(status=message_to_post, in_reply_to_status_id=existing_post['tweet_id'])
+                    else:
+                        # If it doesn't, post a new tweet
+                        tweet = api.update_status(status=message_to_post)
+
+                    # Add the message_to_post to the posts collection
+                    posts_db.insert_one({'pair': doc['pair'], 'timestamp': doc['timestamp'], 'message': message_to_post, 'tweet_id': tweet.id})
+
+    except Exception as e:
+        # Print the error message if something goes wrong
+        print(e)
+
+if __name__ == '__main__':
+    main()
