ba139c162dc1518736942234c0f3a4be2d03cd13
Farkhatzhan Makhmudov <faralost@gmail.com>
2023-01-09 14:45:45 +0600

    add environmental variables

---
+++ config.py
@@ -0,0 +1,14 @@
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+MONGODB_USER = os.environ.get('MONGODB_USER')
+MONGODB_PASSWORD = os.environ.get('MONGODB_PASSWORD')
+MONGODB_CLUSTER_ADDRESS = os.environ.get('MONGODB_CLUSTER_ADDRESS')
+
+TWITTER_CONSUMER_KEY = os.environ.get('TWITTER_CONSUMER_KEY')
+TWITTER_CONSUMER_SECRET = os.environ.get('TWITTER_CONSUMER_SECRET')
+TWITTER_ACCESS_TOKEN = os.environ.get('TWITTER_ACCESS_TOKEN')
+TWITTER_ACCESS_TOKEN_SECRET = os.environ.get('TWITTER_ACCESS_TOKEN_SECRET')
