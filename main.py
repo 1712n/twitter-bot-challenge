e27770ecfd425f0d2eefafc085c97cb22b47298b
immagetmypiece <83126240+immagetmypiece@users.noreply.github.com>
2023-01-11 12:38:29 +0300

    spark test

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 import os
 
-mongodb_user = os.environ["MONGODB_USER"]
-mongodb_password = os.environ["MONGODB_PASSWORD"]
-mongodb_address = os.environ["MONGODB_ADDRESS"]
+import pyspark
+from pyspark.sql import SparkSession
 
-print(mongodb_address)
+mongodb_user = os.environ['MONGODB_USER']
+mongodb_password = os.environ['MONGODB_PASSWORD']
+mongodb_address = os.environ['MONGODB_ADDRESS']
+
+spark = (
+    SparkSession
+    .builder
+    .master('local[*]')
+    .config('spark.mongodb.input.uri', 'mongodb://{mongodb_user}:{mongodb_password}@{mongodb_address}/ohlcv_db')
+    .getOrCreate()
+)
+
+df = spark.read.format('mongodb').load()
+df.printSchema()
