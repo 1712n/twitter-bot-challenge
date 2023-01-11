911066f54a242df183a4e15df366f9105c9793d9
immagetmypiece <83126240+immagetmypiece@users.noreply.github.com>
2023-01-11 15:02:25 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from pyspark.sql import SparkSession
 mongodb_user = os.environ['MONGODB_USER']
 mongodb_password = os.environ['MONGODB_PASSWORD']
 mongodb_address = os.environ['MONGODB_ADDRESS']
+mongodb_uri = 'mongodb://{mongodb_user}:{mongodb_password}@{mongodb_address}'
 
 jars_path = 'jars/*'
 
@@ -13,12 +14,18 @@ spark = (
     SparkSession
     .builder
     .master('local[*]')
-    .config('spark.mongodb.input.uri', 'mongodb://{mongodb_user}:{mongodb_password}@{mongodb_address}')
+    .config('spark.mongodb.input.uri', mongodb_uri)
     .config('spark.mongodb.input.database', 'ohlcv_db')
     .config('spark.mongodb.input.readPreference.name', 'secondaryPreferred')
     .config('spark.driver.extraClassPath', jars_path)
     .getOrCreate()
 )
 
-df = spark.read.format('mongodb').load()
+df = (
+    spark.read
+    #.format('mongodb').load()
+    .format('com.mongodb.spark.sql.DefaultSource')
+    .option('uri', f'{mongodb_uri}/ohlcv_db')
+    .load()
+)
 df.printSchema()
