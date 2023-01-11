587150ecaeb5998afe7629e21dfa938b07655369
immagetmypiece <83126240+immagetmypiece@users.noreply.github.com>
2023-01-11 12:16:27 +0300

    Update main.py
    MONGO_DB_ADDRESS to MONGODB_ADDRESS

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ import os
 
 mongodb_user = os.environ["MONGODB_USER"]
 mongodb_password = os.environ["MONGODB_PASSWORD"]
-mongodb_address = os.environ["MONGO_DB_ADDRESS"]
+mongodb_address = os.environ["MONGODB_ADDRESS"]
 
 print(mongodb_address)
