2d77e580333d2cc2432220f5cd30727d2839af74
immagetmypiece <83126240+immagetmypiece@users.noreply.github.com>
2023-01-11 15:56:16 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,13 +6,7 @@ from pyspark.sql import SparkSession
 mongodb_user = os.environ['MONGODB_USER']
 mongodb_password = os.environ['MONGODB_PASSWORD']
 mongodb_address = os.environ['MONGODB_ADDRESS']
-mongodb_uri = f'mongodb://{mongodb_user}:{mongodb_password}@{mongodb_address}'
-
-from pymongo import MongoClient
-
 mongodb_uri = f'mongodb+srv://{mongodb_user}:{mongodb_password}@{mongodb_address}'
-client = MongoClient(mongodb_uri)
-print(client)
 
 jars_path = 'jars/*'
 
@@ -20,7 +14,7 @@ spark = (
     SparkSession.builder
     .master('local[*]')
     .config('spark.driver.extraClassPath', jars_path)
-    .config('spark.mongodb.read.connection.uri', f'{mongodb_uri}/ohlcv_db.pair')
+    .config('spark.mongodb.read.connection.uri', f'{mongodb_uri}/ohlcv_db.pair?authSource=admin')
     #.config('spark.mongodb.write.connection.uri', 'mongodb://test/test.coll')
     .getOrCreate()
 )
