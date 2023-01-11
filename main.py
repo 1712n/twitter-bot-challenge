539bc9dcfafbf8cee00972ec0971edb4dbda0142
immagetmypiece <83126240+immagetmypiece@users.noreply.github.com>
2023-01-11 12:48:37 +0300

    add jars_path

--- main.py
+++ main.py
@@ -7,11 +7,14 @@ mongodb_user = os.environ['MONGODB_USER']
 mongodb_password = os.environ['MONGODB_PASSWORD']
 mongodb_address = os.environ['MONGODB_ADDRESS']
 
+jars_path = 'jars/*'
+
 spark = (
     SparkSession
     .builder
     .master('local[*]')
     .config('spark.mongodb.input.uri', 'mongodb://{mongodb_user}:{mongodb_password}@{mongodb_address}/ohlcv_db')
+    .config('spark.driver.extraClassPath', jars_path)
     .getOrCreate()
 )
 
