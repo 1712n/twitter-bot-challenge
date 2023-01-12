c1c419304ee7e8807d85c1375e9d9224031967e0
immagetmypiece <83126240+immagetmypiece@users.noreply.github.com>
2023-01-12 14:14:44 +0300

    get top_100_pairs

--- main.py
+++ main.py
@@ -19,15 +19,27 @@ spark = (
     .getOrCreate()
 )
 
-df = (
+ohlcv_df = (
     spark.read
     .format('mongodb')
     .option('database', 'metrics')
     .option('collection', 'ohlcv_db')
     .option('partitioner', 'com.mongodb.spark.sql.connector.read.partitioner.SinglePartitionPartitioner')
     .load()
-    .orderBy(F.desc('volume'))
+)
+
+# check if compound volume should be in pieces or in usd
+# check if last n days should be filtered explicitly
+# check if need to handle the case usd != usdc,usdt...
+# check if top coins should be taken but not pairs
+top_100_pairs = (
+    ohlcv_df
+    .filter('granularity = "1h"') # filter 'today - n days >= timestamp'?
+    .withColumn('compound_volume_usd', F.col('volume')*F.col('close')) # close * stablecoin_rate?
+    .groupBy('pair_base', 'pair_symbol') # pair_symbol only?
+    .agg(F.sum('volume'), F.sum('compound_volume_usd'))
+    .orderBy(F.desc('sum(compound_volume_usd)')) # sum(volume)?
     .limit(100)
 )
-df.printSchema()
-df.show()
+
+top_100_pairs.show(truncate=False)
