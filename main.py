6582b26a8382c9755fc3ba9d1bb16ae4313eb3b9
Braingenius2 <braingenius2@gmail.com>
2023-01-12 11:22:32 +0300

    Update main.py
    use of functions for specific tasks like composing a tweet and posting a tweet

--- main.py
+++ main.py
@@ -2,16 +2,33 @@ import tweepy
 import pymongo
 import os
 
+def post_tweet(pair_to_post, message_to_post):
+    # Authenticate with the Twitter API using OAuth 1.0a
+    auth = tweepy.OAuth1UserHandler(
+        consumer_key=os.environ['TW_CONSUMER_KEY'],
+        consumer_secret=os.environ['TW_CONSUMER_SECRET'],
+        access_token=os.environ['TW_ACCESS_TOKEN'],
+        access_token_secret=os.environ['TW_ACCESS_TOKEN_SECRET'])
+    api = tweepy.API(auth)
+    tweet = api.update_status(status=message_to_post)
+    tweet_id = tweet.id
+
+    # add tweet to posts_db
+    post = {
+        "pair": pair_to_post,
+        "message": message_to_post,
+        "tweet_id": tweet_id
+    }
+    posts_db.insert_one(post)
+
+def compose_message(pair_to_post, volume_data):
+    message = f"Top Market Venues for {pair_to_post}:\n"
+    for market, volume in volume_data:
+        message += f"{market}: {volume}%\n"
+    return message
+
 def main():
     try:
-        # Authenticate with the Twitter API using OAuth 1.0a
-        auth = tweepy.OAuth1UserHandler(
-            consumer_key=os.environ['TW_CONSUMER_KEY'],
-            consumer_secret=os.environ['TW_CONSUMER_SECRET'],
-            access_token=os.environ['TW_ACCESS_TOKEN'],
-            access_token_secret=os.environ['TW_ACCESS_TOKEN_SECRET'])
-        api = tweepy.API(auth)
-
         # Connect to the MongoDB database
         user = os.environ["MONGODB_USER"]
         password = os.environ["MONGODB_PASSWORD"]
@@ -22,31 +39,31 @@ def main():
         ohlcv_db = client['ohlcv_db']
         posts_db = client['posts_db']
 
-        # Use a tailable cursor to efficiently read the latest documents in the ohlcv_db capped collection
-        cursor = ohlcv_db.find(cursor_type=pymongo.cursor.CursorType.TAILABLE).sort([('$natural', -1)]).limit(100)
+        # # Use a tailable cursor to efficiently read the latest documents in the ohlcv_db capped collection
+        # cursor = ohlcv_db.find(cursor_type=pymongo.cursor.CursorType.TAILABLE).sort([('$natural', -1)]).limit(100)
 
-        # Continuously process new documents as they are added to the collection
-        while cursor.alive:
-            for doc in cursor:
-                # Check if the document corresponds to a pair that hasn't been tweeted about recently
-                latest_post = posts_db.find_one({'pair': doc['pair']})
-                if not latest_post or doc['timestamp'] > latest_post['timestamp']:
-                    # Compose the message_to_post
-                    message_to_post = f"Top Market Venues for {doc['pair']}:\n"
-                    for market, volume in doc['market_volumes'].items():
-                        message_to_post += f"{market}: {volume:.2f}%\n"
+        # # Continuously process new documents as they are added to the collection
+        # while cursor.alive:
+        #     for doc in cursor:
+        #         # Check if the document corresponds to a pair that hasn't been tweeted about recently
+        #         latest_post = posts_db.find_one({'pair': doc['pair']})
+        #         if not latest_post or doc['timestamp'] > latest_post['timestamp']:
+        #             # Compose the message_to_post
+        #             message_to_post = f"Top Market Venues for {doc['pair']}:\n"
+        #             for market, volume in doc['market_volumes'].items():
+        #                 message_to_post += f"{market}: {volume:.2f}%\n"
 
-                    # Check if the message_to_post already exists in the posts collection
-                    existing_post = posts_db.find_one({'message': message_to_post})
-                    if existing_post:
-                        # If it does, post the tweet to the corresponding Twitter thread
-                        tweet = api.update_status(status=message_to_post, in_reply_to_status_id=existing_post['tweet_id'])
-                    else:
-                        # If it doesn't, post a new tweet
-                        tweet = api.update_status(status=message_to_post)
+        #             # Check if the message_to_post already exists in the posts collection
+        #             existing_post = posts_db.find_one({'message': message_to_post})
+        #             if existing_post:
+        #                 # If it does, post the tweet to the corresponding Twitter thread
+        #                 tweet = api.update_status(status=message_to_post, in_reply_to_status_id=existing_post['tweet_id'])
+        #             else:
+        #                 # If it doesn't, post a new tweet
+        #                 tweet = api.update_status(status=message_to_post)
 
-                    # Add the message_to_post to the posts collection
-                    posts_db.insert_one({'pair': doc['pair'], 'timestamp': doc['timestamp'], 'message': message_to_post, 'tweet_id': tweet.id})
+        #             # Add the message_to_post to the posts collection
+        #             posts_db.insert_one({'pair': doc['pair'], 'timestamp': doc['timestamp'], 'message': message_to_post, 'tweet_id': tweet.id})
 
     except Exception as e:
         # Print the error message if something goes wrong
