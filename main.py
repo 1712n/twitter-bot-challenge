8bafc6659b852e14052990b34f6981adceedf438
Alexey Gorelov <alek7atw@gmail.com>
2023-01-13 17:40:08 +0400

    refactoring: main and logging added

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 import os
+import logging
 
-from dotenv import load_dotenv
 from pymongo import MongoClient
+from pymongo.errors import PyMongoError
+from dotenv import load_dotenv
 
 import pprint
+
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(format='%(name)s: %(levelname)s: %(message)s', level=logging.DEBUG)
+
 
 def get_top_pairs(pairs_col):
     pairs_total_vol_list = list(pairs_col.aggregate(
@@ -62,21 +69,36 @@ def get_posts_for_pairs(posts_col, pairs_list):
     return posts_list
 
 
-load_dotenv()
+def main():
+    load_dotenv()
 
-user = os.environ["MONGODB_USER"]
-password = os.environ["MONGODB_PASSWORD"]
-address = os.environ["MONGO_DB_ADDRESS"]
+    user = os.environ["MONGODB_USER"]
+    password = os.environ["MONGODB_PASSWORD"]
+    address = os.environ["MONGO_DB_ADDRESS"]
 
-uri = f"mongodb+srv://{user}:{password}@{address}"
-client = MongoClient(uri)
+    uri = f"mongodb+srv://{user}:{password}@{address}"
+    
+    client = MongoClient(uri)
+    try:
+        # The ping command is cheap and does not require auth.
+        client["metrics"].command('ping')
+    except PyMongoError:
+        logging.error("Database connection failure:")
+        raise
 
-ohlcv_col = client["metrics"]["ohlcv_db"]
-posts_col = client["metrics"]["posts_db"]
 
-top_100_pairs_vol = get_top_pairs(ohlcv_col)
+    logger.info("test info")
 
-top_pairs = [x['_id'] for x in top_100_pairs_vol]
+    # ohlcv_col = client["metrics"]["ohlcv_db"]
+    # posts_col = client["metrics"]["posts_db"]
 
-corresponding_posts = get_posts_for_pairs(posts_col, top_pairs)
-pprint.pprint(corresponding_posts)
+    # top_100_pairs_vol = get_top_pairs(ohlcv_col)
+
+    # top_pairs = [x['_id'] for x in top_100_pairs_vol]
+
+    # corresponding_posts = get_posts_for_pairs(posts_col, top_pairs)
+    # pprint.pprint(corresponding_posts)
+
+
+if __name__ == "__main__":
+    main()     
