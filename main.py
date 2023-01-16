4a2f825fe52ebeb190d90212c66ffab99a65b33c
Valeriy Ernandes <lev.370x@gmail.com>
2023-01-16 22:19:35 +0300

    What's the db name?

--- main.py
+++ main.py
@@ -14,11 +14,18 @@ def main():
     log.logger.debug('The app started')
 
     client: MongoClient = get_db()
-    db = client.sample_mflix
-    collection = db.movies
+    try:
+        log.logger.debug('list_databases...')
+        for db in client.list_databases():
+            pprint.pprint(db)
+    except Exception as e:
+        log.logger.debug(f"list_databases... failed: {e}")
 
-    log.logger.debug('find_one ...')
-    pprint.pprint(collection.find_one())
+    # db = client.sample_mflix
+    # collection = db.movies
+
+    # log.logger.debug('find_one ...')
+    # pprint.pprint(collection.find_one())
 
 
 if __name__ == '__main__':
