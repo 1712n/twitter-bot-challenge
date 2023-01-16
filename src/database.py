b847e8e644b7ac61aff2f12771545be5dd5cdd58
ShohKhan-dev <shoh.hamraqulov@gmail.com>
2023-01-16 21:52:50 +0300

    setting up database

--- src/database.py
+++ src/database.py
@@ -3,3 +3,24 @@ from pymongo.errors import PyMongoError
 from config import MONGODB_USER, MONGODB_PASSWORD, MONGODB_CLUSTER_ADDRESS
 import logging
 
+class MongoDatabase:
+    def __init__(self):
+        try:
+            self.cluster = MongoClient(f'mongodb+srv://{MONGODB_USER}:{MONGODB_PASSWORD}@{MONGODB_CLUSTER_ADDRESS}')
+            logging.info('Connected to MongoDB Server.')
+
+        except PyMongoError as e:
+            logging.error(f"Cannot connect to the database: {e}")
+            exit(1)
+
+    def posts(self):
+        self.cluster["metrics"].command('ping')
+        return self.cluster["metrics"]["posts_db"]
+
+    def ohlcv(self):
+        return self.cluster["metrics"]["ohlcv_db"]
+
+
+database = MongoDatabase()
+a = database.posts()
+print(a)
