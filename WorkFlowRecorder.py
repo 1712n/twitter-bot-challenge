2c44e40d5c701843b34e9a9aa4e548369100f2f5
BorisThereIs <boris.thereis@gmail.com>
2023-01-19 02:23:54 +0600

    feat: add new logic in class WorkFlowRecorder

--- WorkFlowRecorder.py
+++ WorkFlowRecorder.py
@@ -1,12 +1,21 @@
 from datetime import datetime
 
 class WorkFlowRecorder():
-    """Makes logs in the meantime of bot execution"""
+    """Makes logs during a run"""
 
-    def __init__(self) -> None:
-        pass
+    def __init__(self, message_separator: str="\n") -> None:
+        """"""
+        self.indent = " "*(len(datetime.utcnow().isoformat())) + " | "
+        self.message_separator = message_separator
 
     def get_logged(self, message: str) -> None:
         """Prints passed messages with a timestamp at that moment to stdout."""
 
-        print(f"{datetime.utcnow().isoformat()} | {message}")
+        call_timestamp = datetime.utcnow()
+        splited_message = message.split(sep=self.message_separator)
+
+        print(f"{call_timestamp.isoformat()} | {splited_message[0]}")
+
+        if len(splited_message) > 1:
+            for i in range(1, len(splited_message)):
+                print(f"{self.indent}{splited_message[i]}")
