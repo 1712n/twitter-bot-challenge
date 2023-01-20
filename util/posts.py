53326cf740893d2d2398b6982d5b0ea9de0b6107
Valeriy Ernandes <lev.370x@gmail.com>
2023-01-20 07:12:28 +0300

    Fixed (workaround) list problem for pair_to_post

--- util/posts.py
+++ util/posts.py
@@ -12,17 +12,19 @@ def select_pair(top_pairs):
     posts_tool = PostsToolBox()
 
     err, oldest_post = posts_tool.get_oldest_pairs_post(top_pairs)
-    if not err:
+    if not err and ('pair' in oldest_post):
         logger.debug(f"We have oldest post: {oldest_post}")
-        pair_to_post = oldest_post['pair']
+        # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        # Sometimes pair is list. I don't know why. It's a workaround
+        if type(oldest_post['pair']) == list:
+            logger.warning(f"Sometimes pair is list: {oldest_post['pair']}")
+            pair_to_post = oldest_post['pair'][0]
+        else:
+            pair_to_post = oldest_post['pair']
         logger.debug(f"pair_to_post: {pair_to_post}")
         return pair_to_post
     else:
         logger.critical("Failed to get oldest post")
         return None
-
-
-def add_message():
-    ...
 
 
