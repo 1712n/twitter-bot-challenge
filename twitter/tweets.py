b95c3acb09a5bb2220816c8c758b61133ad2d0b7
Valeriy Ernandes <lev.370x@gmail.com>
2023-01-20 14:24:01 +0300

    twitter/tweets.py Exceptions handling

--- twitter/tweets.py
+++ twitter/tweets.py
@@ -1,12 +1,10 @@
-#
+# For logging
 import logging
-#
-from requests_oauthlib import OAuth1Session
-import os
+# For parsing response
 import json
 
 # Settings
-from core.config import settings
+# from core.config import settings
 from core.config import APP_NAME
 from twitter.session import tw_session
 
@@ -14,21 +12,36 @@ logger = logging.getLogger(f"{APP_NAME}.{__name__}")
 
 
 class TwitterToolBox:
-    def create_tweet(self, text: str):
+    def create_tweet(self, text: str) -> tuple[str | None, list | None]:
+        """
+        Sent POST request to create a tweet with text
+        :param: text
+        :return: tweet_id
+        """
         # Making the request
+        url = "https://api.twitter.com/2/tweets"
         payload = {"text": text}
-        response = tw_session.oauth.post(
-            "https://api.twitter.com/2/tweets",
-            json=payload,
-        )
+        err = None
+        try:
+            response = tw_session.oauth.post(url=url, json=payload)
+        except Exception as e:
+            err = f"Failed to make a POST request: {url}"
+            logger.critical(err)
+            return err, None
 
-        if response.status_code != 201:
-            raise Exception(
-                f"Request returned an error: {response.status_code} {response.text}"
-            )
         logger.debug(f"Response code: {response.status_code}")
+        if response.status_code != 201:
+            err = f"Request returned an error: {response.status_code} {response.text}"
+            return err, None
 
-        # Saving the response as JSON
-        json_response = response.json()
-        logger.debug(json.dumps(json_response, indent=2, sort_keys=True))
+        try:
+            json_response = response.json()
+            logger.debug(f"Received: {json.dumps(json_response, indent=2, sort_keys=True)}")
+            tweet_id = json_response['data']['id']
+            logger.info(f"Created tweet id: {tweet_id}")
+        except Exception as e:
+            err = "Failed to parse response after POST request"
+            logger.debug(err)
+            return err, None
+        return tweet_id
 
